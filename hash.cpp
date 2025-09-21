8283747e2ac755874bd44a3b6a161a2ba7441892
Gustavas <gustavas.marcinkevicius@gmail.com>
2025-09-21 15:33:17 +0300

    versija v0.1 baigta

--- hash.cpp
+++ hash.cpp
@@ -39,40 +39,67 @@ std::string hash(std::string input, int seed){
         else amount_of_0++;
     }
 
-    int bigger = ((amount_of_0 > amount_of_1) ? amount_of_0 : amount_of_1)*seed;
-    int smaller = ((amount_of_0 < amount_of_1) ? amount_of_0 : amount_of_1)*seed;
-    // std::cout << bigger << " " << smaller << std::endl;
-    // std::cout << "amount of 1, 0:   " << amount_of_1 << "   " << amount_of_0 << std::endl;
-
-    std::cout << input << std::endl;
+    int bigger = ((amount_of_0 > amount_of_1) ? amount_of_0 : amount_of_1)*seed*100;
+    int smaller = ((amount_of_0 < amount_of_1) ? amount_of_0 : amount_of_1);
+    if ((bigger % smaller) == 0)
+        smaller++;
 
 
-    for (int i=0; i<smaller; i++){
-        int current = 0;
+    std::cout << input.length() << std::endl;
+    std::cout << bigger << " " << smaller << std::endl;
+
+    int current = 0;
+    for (int i=0; i<bigger; i++){
         char temp = input[current];
-        int next_pos = (current + bigger - i) % input.length();
+        int next_pos = (current + smaller - i) % input.length();
+        // std::cout << "next_pos: " << next_pos << " current: " << current << " smaller: " << smaller << " i: " << i << std::endl;
         input[current] = input[next_pos];
         input[next_pos] = temp;
         current = next_pos; 
-        // std::cout << input << std::endl;
+        // std::cout << "switching " << current << "with " << next_pos << std::endl; 
     }
-    std::cout << input << std::endl;
+    input = binaryToHex(input);
+    input = wordToBinary(input);
 
-    // Pavertus i binary, 256 1 ir 0
-    std::string Hashed = "qwertyuiopasdfghjklzxcvbnm945137";  
-    Hashed = wordToBinary(Hashed);
+    current = 0;
+    for (int i=0; i<smaller; i++){
+    char temp = input[current];
+    int next_pos = (current + bigger - i) % input.length();
+    input[current] = input[next_pos];
+    input[next_pos] = temp;
+    current = next_pos; 
+    }
+
+    while (input.size() < 256){ //Gali but sitoj vietoj blogai
+        input += input;
+    }
 
 
-    return Hashed;
+
+
+    // Pavertus i binary, 256 bitai
+    std::string start = "qwertyuiopasdfghjklzxcvbnm945137";  
+    start = wordToBinary(start);
+
+    std::string Hashed = "";
+
+    for (int i=0; i<256; i++){
+        if (start[i] == input[i])
+        Hashed += '1';
+        else Hashed += '0';
+    }
+
+    return binaryToHex(Hashed);
+    return input;
 }
 
 int main(){
 
     int seed = 10;
     std::string input = "labaa";
-    hash(input, seed);
-    std::cout << binaryToHex("1100011000010110001001100001011000010110");
+    std::cout << hash("dfasghbjkljuiygtfyghbjuiokpkihyugtrdfyguhijoiadsgyusadaa", seed) << std::endl;
+    std::cout << hash("dfasghbjkljuiygtfaghbjuiokpkihyugtrdfyguhijoiadsgyusadaa", seed) << std::endl;
+
     
-    // std::cout << hash(input) << std::endl;
     return 0;
 }
